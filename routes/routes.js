64ac03f14e680722838dabb08a1f52a18e6f0b16
Matt Bow <mattybow@gmail.com>
2015-12-04 16:47:41 -0600

    support for legacy routes added

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,14 @@
+var LEGACY_ROUTES = {
+  flavors:'flavorsDay',
+  about:'story',
+  contact: 'contact',
+  locations: 'location',
+  press:'press'
+}
+
+var newHash;
+var scrollPending = false;
+
 Router.configure({
   notFoundTemplate: 'notFound'
 });
@@ -11,16 +22,23 @@ Router.route('/',{
 		];
 	},
 	onRun:function(){						//on initial load, will scrollTo hash
-		var hash = window.location.hash;
-		Meteor.setTimeout(function(){
-			viaScrollTo(hash);
-		});
+    if(getUrlFragment()){
+      scrollPending = true;
+    }
 		this.next();
 	},
 	onAfterAction: function(){
-		setAdminTitle('Via Gelato Hawaii')
+		setAdminTitle('Via Gelato Hawaii');
+    console.log('after action');
+    var hash = getUrlFragment();
+    if(scrollPending){
+      Meteor.setTimeout(function(){
+        viaScrollTo(hash);
+      });
+    }
 	},
 	action:function(){
+    console.log('run VIA route');
 		this.render('Via');
 	}
 });
@@ -43,6 +61,22 @@ Router.route('/edit/:tab',{
 	}
 });
 
+Router.route('/:legacyRoute',{
+	action:function(){
+    console.log('detect legacy route');
+    var htmlRoute = this.params.legacyRoute;
+    var htmlRegex = /\.html/;
+    if(htmlRoute.match(htmlRegex)){
+      var route = htmlRoute.replace(htmlRegex,'');
+      if (route in LEGACY_ROUTES){
+        newHash = '#' + LEGACY_ROUTES[route];
+        this.redirect('/'+ newHash);
+      }
+    }
+    this.render('notFound');
+	}
+});
+
 Router.route('/test',function(){
 	this.render('test');
 });
@@ -53,10 +87,18 @@ if(Meteor.isClient){
 function setAdminTitle(title){
 	document.title = title;
 }
+
+function getUrlFragment(){
+  return 	window.location.hash || newHash;
+}
+
 function viaScrollTo(hash) {
   var section = $(hash);
+  console.log(hash, section.length, document.getElementById('flavorsDay'));
   if (section.length) {
     var sectionTop = section.offset().top + 5;
+    scrollPending = false;
+    console.log(sectionTop);
     $("html, body").animate({
       scrollTop: sectionTop
     }, 350,function(){
