afc9f5c966ca3746ea1de4a673b6c8ce9bbe749c
Matt Bow <mattybow@gmail.com>
2015-12-05 14:27:50 -0600

    gift card promo section added

---
+++ client/views/giftCards.js
@@ -0,0 +1,14 @@
+Template.giftCards.helpers({
+  getImgUrl:function(){
+    var url = "https://s3-us-west-2.amazonaws.com/viagelato/images/via_gift_cards_v2";
+    var ext = '.jpg';
+    var resolution = '';
+    if(Meteor.isClient){
+  		var mediaWidth = window.innerWidth;
+  		if(mediaWidth <= MEDIA_BREAK_POINTS.mobile){
+  			resolution = "-mobile"
+  		}
+  	}
+    return url + resolution + ext;
+  }
+})
