16da11722b55c32bf3bb8d18048669f74b2efd4e
Matt Bow <mattybow@gmail.com>
2017-11-13 20:59:48 -0800

    adds new image for ward location

--- client/views/location.js
+++ client/views/location.js
@@ -1,5 +1,5 @@
 Template.location.helpers({
-	getLocationLink:function(){
+	getLink:function(){
 		switch (utils.getMobileOperatingSystem()){
 			case 'iOS':
 				return 'maps://maps.apple.com/?q=Via+Gelato+Hawaii&center=21.2821321,-157.7992884';
@@ -9,41 +9,14 @@ Template.location.helpers({
 				return 'http://maps.google.com/?q=Via+Gelato+Hawaii&center=21.2821321,-157.7992884';
 		}
 	},
-	getViewbox:function(){
-		var mediaWidth = window.innerWidth;
-		if(mediaWidth <= MEDIA_BREAK_POINTS.mobile){
-			return '50 0 640 612';
-		} else if (mediaWidth <= MEDIA_BREAK_POINTS.tablet){
-			return '25 0 700 612';
-		} else {
-			return '0 0 792 612'
+	getWardLink:function(){
+		switch (utils.getMobileOperatingSystem()){
+			case 'iOS':
+				return 'maps://maps.apple.com/?ll=21.292944,-157.852250';
+			/*case 'Android':
+				return 'comgooglemaps://?q=Via+Gelato+Hawaii&center=21.2821321,-157.7992884';*/
+			default:
+				return '//goo.gl/maps/bRgurwEXBeK2';
 		}
-	},
-	fillSvg:function(){
-		return Template.instance().showSvgFill.get() ? 'fill-in' : '';
 	}
 });
-
-Template.location.onCreated(function(){
-	var _self = this;
-	Session.set('drawLocation',false);
-	this.autorun(function(c){
-		if(Session.get('drawLocation')){
-			_self.locationSvg.draw();
-			console.log("DRAW");
-			Meteor.setTimeout(function(){
-				_self.showSvgFill.set(true);
-			},1000);
-			c.stop();
-		}
-	});
-	this.showSvgFill = new ReactiveVar(false);
-});
-
-Template.location.onRendered(function(){
-	this.locationSvg = new Walkway({
-		selector: '#svg-map',
-		duration: '1500'
-	});
-});
-
